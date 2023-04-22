c4de6c5f340d3cee39fe42187c6d6d2b9adda8c7
Sam Peck <fullstacksam@protonmail.com>
2023-04-21 22:56:57 -0400

    yeahhh

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+
+
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "SAM-PECK is the champion";
+
+//
+//newHeader varaibble that points to the h1 has an id of victory
+//
